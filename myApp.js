300337e4a3bbca118f5445e80993320e670c7bfe
Yann G <yann.gedon@hotmail.fr>
2022-01-25 10:30:48 +0100

    add get/now function

--- myApp.js
+++ myApp.js
@@ -5,6 +5,12 @@ app.get('/json',function(req,res,next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
 });
+app.get('/now',function(req,res,next) {
+  req.time = new Date().toString();
+  next();
+}, function (req,res){
+  req.setEncoding({time : req.time})
+});
 app.use('/public',express.static(__dirname + "/public"));
 app.use('/views',express.static(__dirname + "/views"));
 
