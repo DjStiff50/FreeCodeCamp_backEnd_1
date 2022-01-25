12da83b198b6687b4fe391c9150ce0259700db83
Yann G <yann.gedon@hotmail.fr>
2022-01-25 11:07:26 +0100

    a

--- myApp.js
+++ myApp.js
@@ -5,10 +5,10 @@ var app = express();
 app.use('/public',express.static(__dirname + "/public"));
 app.use('/views',express.static(__dirname + "/views"));
 
-app.get('/json',function(req,res,next) {
+/*app.get('/json',function(req,res,next) {
   console.log(req.method + " " + req.path + " - " + req.ip);
   next();
-});
+});*/
 app.get(
   "/now",
   (req, res, next) => {
